cfa0b2d00dc2c8e5e0fd02f9051720c050413fcd
Toyoda Hideyuki <hide.toyoda@gmail.com>
2024-06-14 21:44:47 +0900

    commit 20240614

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  final List<GlobalKey> globalKeyList = [GlobalKey(), GlobalKey(), GlobalKey()];
+  List<GlobalKey> globalKeyList = [];
 
   ScrollController scrollController = ScrollController();
 
   ///
   Future<void> scrollToIndex(int index) async {
-    final categories = globalKeyList[index].currentContext!;
+    final target = globalKeyList[index].currentContext!;
 
     await Scrollable.ensureVisible(
-      categories,
+      target,
       duration: const Duration(milliseconds: 1000),
     );
   }
@@ -20,54 +20,54 @@ class HomeScreen extends StatelessWidget {
   ///
   @override
   Widget build(BuildContext context) {
+    globalKeyList = List.generate(10, (index) => GlobalKey());
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      scrollToIndex(2);
+      scrollToIndex(7);
     });
 
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () => scrollToIndex(0), icon: const Icon(Icons.ac_unit)),
+              onPressed: () => scrollToIndex(0),
+              icon: const Icon(Icons.ac_unit)),
           IconButton(
-              onPressed: () => scrollToIndex(1), icon: const Icon(Icons.ac_unit)),
+              onPressed: () => scrollToIndex(1),
+              icon: const Icon(Icons.ac_unit)),
           IconButton(
-              onPressed: () => scrollToIndex(2), icon: const Icon(Icons.ac_unit)),
+              onPressed: () => scrollToIndex(2),
+              icon: const Icon(Icons.ac_unit)),
+          IconButton(
+              onPressed: () => scrollToIndex(3),
+              icon: const Icon(Icons.ac_unit)),
+          IconButton(
+              onPressed: () => scrollToIndex(4),
+              icon: const Icon(Icons.ac_unit)),
+          IconButton(
+              onPressed: () => scrollToIndex(5),
+              icon: const Icon(Icons.ac_unit)),
         ],
       ),
       body: SingleChildScrollView(
         controller: scrollController,
         child: Column(
-          children: [
-            _buildCategoryTitle(0),
-            Container(height: 1000),
-            _buildCategoryTitle(1),
-            Container(height: 1000),
-            _buildCategoryTitle(2),
-            Container(height: 1000),
-            const SizedBox(height: 30)
-          ],
+          children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((e) {
+            return Column(
+              children: [
+                Row(
+                  key: globalKeyList[e],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(e.toString(), style: const TextStyle(fontSize: 30)),
+                    Container(),
+                  ],
+                ),
+                Container(height: 1000),
+              ],
+            );
+          }).toList(),
         ),
-      ),
-    );
-  }
-
-  ///
-  Widget _buildCategoryTitle(int index) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.redAccent),
-      ),
-      child: Row(
-        key: globalKeyList[index],
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            index.toString(),
-            style: const TextStyle(fontSize: 30),
-          ),
-          Container(),
-        ],
       ),
     );
   }
