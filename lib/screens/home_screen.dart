a418832cacf82b081d0a962e15a461da1c32490c
Toyoda Hideyuki <hide.toyoda@gmail.com>
2024-06-22 10:03:02 +0900

    commit 20240622

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,55 +21,62 @@ class HomeScreen extends StatelessWidget {
   ///
   @override
   Widget build(BuildContext context) {
-    globalKeyList = List.generate(10, (index) => GlobalKey());
+    globalKeyList = List.generate(20, (index) => GlobalKey());
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       scrollToIndex(7);
     });
 
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () => scrollToIndex(0),
-              icon: const Icon(Icons.ac_unit)),
-          IconButton(
-              onPressed: () => scrollToIndex(1),
-              icon: const Icon(Icons.ac_unit)),
-          IconButton(
-              onPressed: () => scrollToIndex(2),
-              icon: const Icon(Icons.ac_unit)),
-          IconButton(
-              onPressed: () => scrollToIndex(3),
-              icon: const Icon(Icons.ac_unit)),
-          IconButton(
-              onPressed: () => scrollToIndex(4),
-              icon: const Icon(Icons.ac_unit)),
-          IconButton(
-              onPressed: () => scrollToIndex(5),
-              icon: const Icon(Icons.ac_unit)),
-        ],
-      ),
-      body: SingleChildScrollView(
-        controller: scrollController,
-        child: Column(
-          children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((e) {
-            return Column(
-              children: [
-                Row(
-                  key: globalKeyList[e],
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(e.toString(), style: const TextStyle(fontSize: 30)),
-                    Container(),
-                  ],
-                ),
-                Container(height: 1000),
-              ],
-            );
-          }).toList(),
+    return DefaultTabController(
+      length: 15,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            isScrollable: true,
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.transparent,
+            padding: EdgeInsets.zero,
+            tabs: _getTabs(),
+          ),
+        ),
+        body: SingleChildScrollView(
+          controller: scrollController,
+          child: Column(
+            children:
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((e) {
+              return Column(
+                children: [
+                  Row(
+                    key: globalKeyList[e],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(e.toString(), style: const TextStyle(fontSize: 30)),
+                      Container(),
+                    ],
+                  ),
+                  Container(height: 1000),
+                ],
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
+  }
+
+  List<Widget> _getTabs() {
+    List<Widget> list = [];
+
+    for (var i = 0; i < 15; i++) {
+      list.add(
+        TextButton(
+          onPressed: () {
+            scrollToIndex(i);
+          },
+          child: Text(i.toString()),
+        ),
+      );
+    }
+    return list;
   }
 }
